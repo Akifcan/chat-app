15b7fef26ef6cdccef922c12f52f7dfea89d0926
Akifcan <akfkara97@gmail.com>
2024-08-31 00:39:47 +0300

    feat(chat): not found message added

--- app/components/chat/conversation-types/not-found-command.message.tsx
+++ app/components/chat/conversation-types/not-found-command.message.tsx
@@ -1,13 +1,9 @@
-import styled from "styled-components";
-
-const Alert = styled.div``;
-
 export default function NotFoundCommandMessage(): JSX.Element {
   return (
     <div className="flex column" style={{ gap: "1rem" }}>
       <p>
-        Please write <b className="selectable-text">/help</b> if you need
-        support.
+        Sorry I didn&apos;t understand. Please write
+        <b className="selectable-text">/help</b> if you need support.
       </p>
     </div>
   );
